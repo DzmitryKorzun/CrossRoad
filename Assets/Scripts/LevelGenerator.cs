0dcd3607c8e94e383dcc29b2aec7310f284b2198
DzmitryKorzun <it.dzmitrykorzun@gmail.com>
2021-04-17 02:05:15 +0300

    add models

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] let = new GameObject[10];
+    public GameObject[] platforms = new GameObject[5];
+    
+
+    void Start()
+    {
+        Instantiate(let[0]);
+        let[0].transform.position = new Vector3(4, 0.01f, 0);
+        Instantiate(let[0]);
+        let[0].transform.position = new Vector3(-4, 0.01f, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+}
